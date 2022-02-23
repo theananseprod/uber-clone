e354e26c0f95b4d85f431fd79a118db364d1efd6
The Ananse Productions <71141965+theananseprod@users.noreply.github.com>
2022-02-23 17:07:01 -0500

    original app creation via expo
    project creation via expo init

---
+++ App.js
@@ -0,0 +1,62 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './screens/HomeScreen';
+//import { Provider } from 'react-redux';
+//import { store } from './store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
+import EatsScreen from './screens/EatsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    //<Provider stores={store}>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="MapScreen"
+            component={MapScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          {/* <Stack.Screen
+            name="EatsScreen"
+            component={EatsScreen}
+            options={{
+              headerShown: false,
+            }}
+          /> */}
+          {/* <Stack.Screen
+            name="RidesScreen"
+            component={RidesScreen}
+            options={{
+              headerShown: false,
+            }}
+          /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+    //</Provider>  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
